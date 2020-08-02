e0b62d9524ae0c3e0f38801ffd2a13e2b455ebee
akimoto1027 <64831697+akimoto1027@users.noreply.github.com>
2020-08-02 14:24:44 +0900

    新規登録・編集・削除機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,34 @@
 class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:company, :site, :place, :phase, :date, :url, :hp, :others, :features)
+  end
+
 end
