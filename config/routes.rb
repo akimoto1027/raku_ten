e4d5b942588e66d6109868af69c17cfc664afd3e
akimoto1027 <64831697+akimoto1027@users.noreply.github.com>
2020-08-09 13:39:28 +0900

    Update application

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'posts#top'
+
   resources :posts do
     collection do
       get 'list', to: 'posts#list'
+      get 'top', to: 'posts#top'
     end
   end
   resources :users, only: :show
